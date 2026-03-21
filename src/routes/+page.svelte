c0441752b384acdd9fe5c2e8776bcd95ed5ab9d7
lycopod <luongcaophong1312@gmail.com>
2026-03-21 23:48:13 +0700

    link to old sites

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <h1>about time i start working on this website...</h1>
-<a href="https://old.lycopod.net">see old site?</a>
+<p>
+    <a href="https://old.lycopod.net">see old site?</a>
+    (check out <a href="https://old.lycopod.net/springs">/springs</a>, it's draggable!)
+</p>
+<p>
+    <a href="https://old2.lycopod.net">see older site?</a>
+    (check out <a href="https://old2.lycopod.net/softbody">/softbody</a> and <a href="https://old2.lycopod.net/fishes">/fishes</a>)
+</p>
 
 <h1>blog</h1>
 {#each data.posts as post}
